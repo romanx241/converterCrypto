3e9cd07b2302a5f3b7c4a6370953f8f1b7d1d44a
romanx241 <hohlovr@mail.ru>
2023-01-09 18:31:46 +0300

    first commit

---
+++ app/src/main/java/ru/netology/convertercryptoapp/CoinPriceListActivity.kt
@@ -0,0 +1,35 @@
+package ru.netology.convertercryptoapp
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ru.netology.convertercryptoapp.adapters.CoinInfoAdapter
+import ru.netology.convertercryptoapp.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.activity_coin_prce_list.*
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_prce_list)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
+        })
+    }
+}
---
+++ app/src/main/java/ru/netology/convertercryptoapp/database/CoinPriceInfoDao.kt
@@ -0,0 +1,20 @@
+package ru.netology.convertercryptoapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.netology.convertercryptoapp.pojo.CoinPriceInfo
+
+@Dao
+interface CoinPriceInfoDao {
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC")
+    fun getPriceList(): LiveData<List<CoinPriceInfo>>
+
+    @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym LIMIT 1")
+    fun getPriceInfoAboutCoin(fSym: String): LiveData<CoinPriceInfo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertPriceList(priceList: List<CoinPriceInfo>)
+}
